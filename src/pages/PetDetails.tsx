4617a207d74458a0844a181d56d277604b4c30e5
OleksandrDarchyk <oledar01@easv365.dk>
2025-08-21 13:52:28 +0200

    Added Toggle Sold button in PetDetails

--- src/pages/PetDetails.tsx
+++ src/pages/PetDetails.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../api/client";
+import type {AxiosError} from "axios";
+
 
 type Pet = {
     id?: string;
@@ -14,6 +16,8 @@ export default function PetDetails() {
     const { petId } = useParams();
     const [pet, setPet] = useState<Pet | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [saving, setSaving] = useState<boolean>(false);
+
 
     useEffect(() => {
         if (!petId) return;
@@ -37,6 +41,29 @@ export default function PetDetails() {
     if (error) return <p style={{ color: "crimson" }}>{error}</p>;
     if (!pet) return <p>Loading...</p>;
 
+    async function handleToggleSold() {
+        if (!pet?.id) return;
+
+        const newSoldStatus = !pet.sold;
+        setSaving(true);
+        try {
+            const res = await api.updatePet.petUpdatePet({
+                id: pet.id,
+                name: pet.name,
+                breed: pet.breed,
+                imgurl: pet.imgurl,
+                sold: newSoldStatus,
+            });
+            setPet(res.data); // оновлюємо стан фактичними даними із сервера
+        } catch (e) {
+            const err = e as AxiosError<{ title?: string }>;
+            alert(err.response?.data?.title ?? "Failed to update pet");
+        } finally {
+            setSaving(false);
+        }
+    }
+
+
     return (
         <div>
             <h2>Pet Details</h2>
@@ -49,6 +76,11 @@ export default function PetDetails() {
                     <img src={pet.imgurl} alt={pet.name ?? "Pet"} style={{ maxWidth: 320 }} />
                 </div>
             )}
+            <button className={`btn ${pet?.sold ? "btn-warning":"btn-success"} mr-2`}
+                    disabled={saving}
+                    onClick={handleToggleSold}>
+                {saving ? "Saving..." : pet.sold ? "Mark as Available" : "Mark as Sold"}
+            </button>
         </div>
     );
 }
